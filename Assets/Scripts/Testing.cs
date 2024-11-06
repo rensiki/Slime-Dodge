0f3ac432f9b51e12a0ad8c92f63f837fa60cefc0
KimTaeMin <ktm051113@naver.com>
2024-11-07 01:37:08 +0900

    timer debugging

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -6,16 +6,15 @@ public class Testing : MonoBehaviour
 {
 
     Rigidbody2D rigid;
-    Transform trans;
 
-    public int destination = 1;
-    public float speed = 0.001f;
-    public float a = 0.1f;
+    public int xMoveValue = 1;
+    public float speed = 0.1f;
+    public float a = 5f;
+    public float movingDelayTime = 0.01f;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        trans = GetComponent<Transform>();
         //StartCoroutine(Test());
         
 
@@ -25,31 +24,53 @@ public class Testing : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Space");
-            Moving(transform.position.x, destination);
+            StartCoroutine(MovingFunc(transform.position.x, xMoveValue, transform.position.y, getAbyxMove()));
         }
     }
 
-    void Moving(float origin, float destination)
-    {
-        float x = origin;
-        while (origin < destination)
+    float getAbyxMove(){
+        if (xMoveValue == 1 || xMoveValue == -1)
         {
-            x += speed;
-            float y = -a * (x - origin) * (x - (origin + destination)) + 1;
+            return a;
+        }
+        else if (xMoveValue == 2 || xMoveValue == -2)
+        {
+            return a * 0.5f;
+        }
+        else if (xMoveValue == 3 || xMoveValue == -3)
+        {
+            return a * 0.3f;
+        }
+        else if (xMoveValue == 4 || xMoveValue == -4)
+        {
+            return a * 0.2f;
+        }
+        return 0;//에러케이스
+        Debug.Log("xMove 범위 에러");
+    }
+
+
+
+    IEnumerator MovingFunc(float origin, float xMoveValue, float Yorigin, float A = 1, float x=0, float y=0)  
+    {
+        while ((xMoveValue>0&&x < xMoveValue) || (xMoveValue < 0 && x > xMoveValue))
+        {
+            x += speed * xMoveValue;//2차함수에서의 점의 x축 방향 이동
+            y = -A * (x - 0) * (x - (xMoveValue)); 
+            //Debug.Log("x : " + (origin + x) + " y : " + (Yorigin + y));
 
             // 오브젝트 위치 갱신
-            transform.position = new Vector3(x, y, transform.position.z);
+            transform.position = new Vector3(origin + x, Yorigin + y, transform.position.z);
+            yield return new WaitForSeconds(movingDelayTime);
         }
+        //약간의 오차를 의도했던 값으로 이동시킴으로써 조정
+        transform.position = new Vector3(origin + xMoveValue, Yorigin, transform.position.z);
     }
+    
     /*IEnumerator Test()
     {
         trans.position = Vector3.Slerp(trans.position, new Vector3(testing, 0.01f, 0), 0.3f);
         yield return new WaitForSeconds(0.1f);
         StartCoroutine(Test());
     }*/
-
-
-
-
-
 }
